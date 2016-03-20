d28842c4368dd9e3b8f311c1662da58122369b69
Askupa Software <contact@askupasoftware.com>
2016-03-20 01:08:02 -0500

    Added comments to functions

--- bootstrap.php
+++ bootstrap.php
@@ -24,6 +24,14 @@ require_once 'renderer.php';
 
 if( !function_exists('wp_dynamic_css_enqueue') )
 {
+    /**
+     * Enqueue a dynamic stylesheet
+     * 
+     * This will print the compiled version of the stylesheet to the document's
+     * <head> section.
+     * 
+     * @param string $path The absolute path to the dynamic CSS file
+     */
     function wp_dynamic_css_enqueue( $path )
     {
         $dcss = DynamicCSSRenderer::get_instance();
@@ -33,6 +41,17 @@ if( !function_exists('wp_dynamic_css_enqueue') )
 
 if( !function_exists('wp_dynamic_css_set_callback') )
 {
+    /**
+     * Set the value retrieval callback function
+     * 
+     * Set a callback function that will be used to get the values of the 
+     * variables when the dynamic CSS file is compiled. The function accepts 1 
+     * parameter which is the name of the variable, without the $ sign
+     * 
+     * @param string|array $callback A callback (or "callable" as of PHP 5.4) 
+     * can either be a reference to a function name or method within an 
+     * class/object.
+     */
     function wp_dynamic_css_set_callback( $callback )
     {
         add_filter( 'wp_dynamic_css_get_variable_value', $callback );
